d8d936a05c69ad2fb3874671d7646fee44fa007f
galaterro <adrix1992@gmail.com>
2016-04-04 16:34:10 +0200

    Código con la función de medir distancia

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,10 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+int trig = 10;
+int echo = 11;
+float distancia;
 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
 }
 
 void loop() {
-  Serial.println("Hola mundo");
-  delay(2000);
 
+  medirDistancia() {
+    //Inicializamos el sensor
+    digitalWrite(trig, LOW);
+    delayMicroseconds(5);
+
+    //Comenzamos las mediciones
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+
+    distancia = pulseIn(echo, HIGH);
+    distancia = distancia * 0.0001657;
+
+    if (distancia < 0.10)
+    {
+      parar();
+      Serial.println("Parar");
+    }
+  }
+
+  
 }
