6ebb3dab7461361e6424e9ba4fe2ffa4f3945308
victAll <vitormanuel.allende@gmail.com>
2016-04-04 18:01:11 +0200

    Cambiado las funciones de moverse.
    Ahora las motores se mueven por pares, avanzando y retrocediendo.

--- Coche/Coche.ino
+++ Coche/Coche.ino
@@ -1,7 +1,8 @@
-int motorDelanteIzq = 4;
-int motorDelanteDcha = 5;
-int motorTrasIzq = 6;
-int motorTrasDcha = 7;
+int motoresIzquierdosAvanzar = 4;
+int motoresIzquierdosRetroceder = 5;
+int motoresDerechosAvanzar = 6;
+int motoresDerechosRetroceder = 7;
+
 int ledEncendido = 8;
 int trig = 10;
 int echo = 11;
@@ -13,12 +14,20 @@ void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(ledEncendido, OUTPUT);
+  pinMode(motoresIzquierdosAvanzar, OUTPUT);
+  pinMode(motoresIzquierdosRetroceder, OUTPUT);
+  pinMode(motoresDerechosAvanzar, OUTPUT);
+  pinMode(motoresDerechosRetroceder, OUTPUT);
 }
 
 void loop() {
-  encenderCoche();
-  avanzarCoche();  
-}
+  //encenderCoche();
+  //girarCocheDcha();  
+  girarCocheIzq();
+  //digitalWrite(motoresIzquierdosRetroceder, HIGH);
+  //digitalWrite(motoresIzquierdosAvanzar, HIGH);
+  //digitalWrite(motoresDerechosRetro, HIGH);
+ }
 
 /**
    * Función para encender el coche. Enciende el Led de Encendido.
@@ -38,40 +47,42 @@ void loop() {
    * Función para hacer que el coche se mueva hacia delante.
    */
   void avanzarCoche() {
-    digitalWrite(motorDelanteIzq, HIGH);
-    digitalWrite(motorDelanteDcha, HIGH);
-    digitalWrite(motorTrasIzq, HIGH);
-    digitalWrite(motorTrasDcha, HIGH);
+    digitalWrite(motoresIzquierdosAvanzar, HIGH);
+    digitalWrite(motoresDerechosAvanzar, HIGH);
+  }
+
+  /**
+   * Función para hacer que el coche se mueva hacia delante.
+   */
+  void retrocederCoche() {
+    digitalWrite(motoresIzquierdosRetroceder, HIGH);
+    digitalWrite(motoresDerechosRetroceder, HIGH);
   }
 
    /**
    * Función para hacer que el coche se pare.
    */
   void pararCoche() {
-    digitalWrite(motorDelanteIzq, LOW);
-    digitalWrite(motorDelanteDcha, LOW);
-    digitalWrite(motorTrasIzq, LOW);
-    digitalWrite(motorTrasDcha, LOW);
-  }
-
-   /**
-   * Función para hacer que el coche gire hacia la izquierda.
-   */
-  void girarCocheIzq() {
-    digitalWrite(motorDelanteIzq, HIGH);
-    digitalWrite(motorDelanteDcha, LOW);
-    digitalWrite(motorTrasIzq, HIGH);
-    digitalWrite(motorTrasDcha, LOW);
+    digitalWrite(motoresIzquierdosAvanzar, LOW);
+    digitalWrite(motoresDerechosAvanzar, LOW);
+    digitalWrite(motoresIzquierdosRetroceder, LOW);
+    digitalWrite(motoresDerechosRetroceder, LOW);
   }
 
    /**
    * Función para hacer que el coche gire hacia la izquierda.
    */
   void girarCocheDcha() {
-    digitalWrite(motorDelanteIzq, LOW);
-    digitalWrite(motorDelanteDcha, HIGH);
-    digitalWrite(motorTrasIzq, LOW);
-    digitalWrite(motorTrasDcha, HIGH);
+    digitalWrite(motoresIzquierdosAvanzar, HIGH);
+    digitalWrite(motoresDerechosRetroceder, HIGH);
+  }
+
+   /**
+   * Función para hacer que el coche gire hacia la izquierda.
+   */
+  void girarCocheIzq() {
+    digitalWrite(motoresDerechosAvanzar, HIGH);
+    digitalWrite(motoresIzquierdosRetroceder, HIGH);
   }
   
   /**
